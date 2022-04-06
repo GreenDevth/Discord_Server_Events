97094ad9adc0078b42f8fbefa349d8b1f981f1e3
GreenDevth <theerapong@rumail.ru.ac.th>
2022-04-06 22:51:12 +0700

    add final location fix steam

--- event/help_me_please.py
+++ event/help_me_please.py
@@ -225,7 +225,7 @@ class HelpMePlease(commands.Cog):
     async def final_place(self, ctx):
         run = self.bot.get_channel(927796274676260944)
         member = ctx.author
-        steam = players_event_info(member.id)[3]
+        steam = players_info(member.id)[3]
         teleport = "#Teleport 175201.875 -199213.156 29079.803"
         await ctx.reply('ระบบกำลังส่งคุณไปยังจุดเส้นชัย', mention_author=False)
         await run.send(f'.set {teleport} {steam}')
